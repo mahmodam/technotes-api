c5d5585e8017720324bf9f80e9ecf4e9253d8023
mahmodam <91775238+mahmodam@users.noreply.github.com>
2022-11-05 17:13:04 +0200

    update 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,9 @@
 // localhost:3000 כדי לאפשר ל react לשלוח בקשות לשרת
 // 'http://localhost:3000' לא צריך יותר אותו כדי שלא יוכלו לגשת אליו
 //  'https://technotes-1bga.onrender.com' שם האתר שלנו שהעליתי ל render
-const allowedOrigins = ["https://technotes-1bga.onrender.com"];
+const allowedOrigins = [
+  "https://technotes-1bga.onrender.com",
+  "http://localhost:3000",
+];
 
 module.exports = allowedOrigins;
